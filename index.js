c591a5602a93dc87a3b09f6e9f9670307545ecc9
bobbyhadz <opmftw@gmail.com>
2024-03-05 08:57:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const box = document.getElementById('box');
+
+if (!box.classList.contains('bg-salmon')) {
+  console.log('Element does NOT have class');
+} else {
+  console.log('Element has class');
+}
